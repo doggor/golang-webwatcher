3ab03a053393a7e3f8757d02b3b1d8faa4f4c2fa
Andy <dogdoglization@gmail.com>
2019-11-01 12:38:54 +0800

    imrove main flow

--- cmd/webwatcher/main.go
+++ cmd/webwatcher/main.go
@@ -12,28 +12,34 @@ import (
 )
 
 func main() {
-	fmt.Println("Program Start.")
+	//log program life status
+	fmt.Println("Chick wakeup")
+	defer fmt.Println("Chick sleep")
 
 	//schedule jobs
 	schedule := cron.New(cron.WithLocation(time.FixedZone("UTC+8", 8*60*60)))
-	schedule.AddFunc("0 9 * * *", jobs.CheckNaebaBusBookingDates) //run at 10:00 daily
+	entryID, err := schedule.AddFunc("0 10 * * *", jobs.CheckNaebaBusBookingDates) //run at 10:00 daily
+
+	//start schedule
 	schedule.Start()
+	defer func() {
+		//wait for jobs completed
+		<-schedule.Stop().Done()
+	}()
 
-	waitForInterrupt()
+	//log job status
+	if err != nil {
+		fmt.Println("Fail to schedule Naeba bus booking checker:", err)
+	} else {
+		fmt.Println("The next time checking Naeba bus booking is", schedule.Entry(entryID).Next)
+	}
 
-	//wait for jobs completed
-	<-schedule.Stop().Done()
-
-	fmt.Println("Program Exit.")
-}
-
-func waitForInterrupt() {
+	//wait for system interrupt
 	systemSignalChan := make(chan os.Signal)
-
 	signal.Notify(systemSignalChan)
-
 	for {
-		if s := <-systemSignalChan; s == syscall.SIGINT || s == syscall.SIGTERM {
+		s := <-systemSignalChan
+		if s == syscall.SIGINT || s == syscall.SIGTERM {
 			break
 		}
 	}
